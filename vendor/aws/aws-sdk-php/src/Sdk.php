0a7f2b280cb797116fa6fa24395710d438a61da2
keiserjb <keiserjb@yahoo.com>
2025-05-22 10:57:27 -0500

    bump aws-sdk-php to 3.343.16

--- vendor/aws/aws-sdk-php/src/Sdk.php
+++ vendor/aws/aws-sdk-php/src/Sdk.php
@@ -318,6 +318,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionFreeTier(array $args = [])
  * @method \Aws\GameLift\GameLiftClient createGameLift(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionGameLift(array $args = [])
+ * @method \Aws\GameLiftStreams\GameLiftStreamsClient createGameLiftStreams(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionGameLiftStreams(array $args = [])
  * @method \Aws\GeoMaps\GeoMapsClient createGeoMaps(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionGeoMaps(array $args = [])
  * @method \Aws\GeoPlaces\GeoPlacesClient createGeoPlaces(array $args = [])
@@ -378,6 +380,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionIoTFleetWise(array $args = [])
  * @method \Aws\IoTJobsDataPlane\IoTJobsDataPlaneClient createIoTJobsDataPlane(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionIoTJobsDataPlane(array $args = [])
+ * @method \Aws\IoTManagedIntegrations\IoTManagedIntegrationsClient createIoTManagedIntegrations(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionIoTManagedIntegrations(array $args = [])
  * @method \Aws\IoTSecureTunneling\IoTSecureTunnelingClient createIoTSecureTunneling(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionIoTSecureTunneling(array $args = [])
  * @method \Aws\IoTSiteWise\IoTSiteWiseClient createIoTSiteWise(array $args = [])
@@ -588,8 +592,6 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionPolly(array $args = [])
  * @method \Aws\Pricing\PricingClient createPricing(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionPricing(array $args = [])
- * @method \Aws\PrivateNetworks\PrivateNetworksClient createPrivateNetworks(array $args = [])
- * @method \Aws\MultiRegionClient createMultiRegionPrivateNetworks(array $args = [])
  * @method \Aws\PrometheusService\PrometheusServiceClient createPrometheusService(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionPrometheusService(array $args = [])
  * @method \Aws\Proton\ProtonClient createProton(array $args = [])
@@ -660,6 +662,8 @@ namespace Aws;
  * @method \Aws\MultiRegionClient createMultiRegionS3Tables(array $args = [])
  * @method \Aws\SSMContacts\SSMContactsClient createSSMContacts(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionSSMContacts(array $args = [])
+ * @method \Aws\SSMGuiConnect\SSMGuiConnectClient createSSMGuiConnect(array $args = [])
+ * @method \Aws\MultiRegionClient createMultiRegionSSMGuiConnect(array $args = [])
  * @method \Aws\SSMIncidents\SSMIncidentsClient createSSMIncidents(array $args = [])
  * @method \Aws\MultiRegionClient createMultiRegionSSMIncidents(array $args = [])
  * @method \Aws\SSMQuickSetup\SSMQuickSetupClient createSSMQuickSetup(array $args = [])
@@ -809,7 +813,7 @@ namespace Aws;
  */
 class Sdk
 {
-    const VERSION = '3.340.1';
+    const VERSION = '3.343.16';
 
     /** @var array Arguments for creating clients */
     private $args;
